132db0f04925d5dcd593cbc861f06cf8b54d704b
Raghav Sharma <48849683+RaghavSharma0007@users.noreply.github.com>
2019-04-22 18:13:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ a valid WWO API key for retrieving weather up to 14 days in the future. Get an
 WWO API key here: https://developer.worldweatheronline.com/api/
 """
 
-WWO_API_KEY = '<INSERT_WWWO_API_KEY_HERE>'
+WWO_API_KEY = '<e4d6a2b4fb7a41fb81f72102191604>'
 MAX_FORECAST_LEN = 13
 _DEFAULT_TEMP_UNIT = 'F'
 
